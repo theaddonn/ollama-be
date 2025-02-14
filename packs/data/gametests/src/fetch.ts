33c1b69759d3c7ebe7d4a1b8869b9165c2649964
theaddon <theaddonn@gmail.com>
2025-02-14 23:00:21 +0100

    Clean up fetch impl

--- packs/data/gametests/src/fetch.ts
+++ packs/data/gametests/src/fetch.ts
@@ -1,63 +1,90 @@
-import { http, HttpRequest, HttpRequestMethod } from '@minecraft/server-net';
+import * as minecraftnet from '@minecraft/server-net';
 
 export const fetch = async (
   input: RequestInfo | URL,
-  init?: RequestInit
+  init?: RequestInit,
 ): Promise<Response> => {
-  if (input instanceof URL) {
-    const response = await http.get(input.host);
+  const request = requestFetch2MC(input, init);
+  const response = responseMC2Fetch(await minecraftnet.http.request(request));
 
-    return new Response(response.body, {
-      status: response.status,
-      headers: response.headers.map((header): [string, string] => {
-        if (header.value instanceof String) {
-          return [header.key, header.value.toString()];
-        } else {
-          return [header.key, ''];
-        }
-      }),
+  return response;
+};
+
+function addInitBody(request: minecraftnet.HttpRequest, init: RequestInit) {
+  if (typeof init.body === 'string') {
+    request.body = init.body;
+  } else if (!(init.body === undefined && init.body === null)) {
+    throw new Error(`Unknown init body type ${init.body}`);
+  }
+}
+
+function addInitHeaders(request: minecraftnet.HttpRequest, init: RequestInit) {
+  if (init.headers !== undefined) {
+    request.headers = [];
+
+    init.headers.forEach((key: string, value: string) => {
+      request.headers.push(new minecraftnet.HttpHeader(key, value));
     });
-  } else if (typeof input === 'string') {
-    let request = new HttpRequest(input.toString());
+  }
+}
 
-    if (init !== undefined) {
-      if (typeof init.body === 'string') {
-        request.body = init.body;
-      }
+function addInitMethod(request: minecraftnet.HttpRequest, init: RequestInit) {
+  const methodMap: Record<string, minecraftnet.HttpRequestMethod> = {
+    GET: minecraftnet.HttpRequestMethod.Get,
+    POST: minecraftnet.HttpRequestMethod.Post,
+    DELETE: minecraftnet.HttpRequestMethod.Delete,
+    HEAD: minecraftnet.HttpRequestMethod.Head,
+    PUT: minecraftnet.HttpRequestMethod.Put,
+  };
 
-      const methodMap: Record<string, HttpRequestMethod> = {
-        GET: HttpRequestMethod.Get,
-        POST: HttpRequestMethod.Post,
-        DELETE: HttpRequestMethod.Delete,
-        HEAD: HttpRequestMethod.Head,
-        PUT: HttpRequestMethod.Put,
-      };
+  if (init.method !== undefined) {
+    const method: minecraftnet.HttpRequestMethod | undefined =
+      methodMap[init.method];
 
-      if (init.method !== undefined) {
-        const method: HttpRequestMethod | undefined = methodMap[init.method];
-
-        if (method === undefined) {
-          throw new Error(`Unknown HTTP Request Method ${init.method}`);
-        }
-
-        request.method = method;
-      }
+    if (method === undefined) {
+      throw new Error(`Unknown HTTP Request Method ${init.method}`);
     }
 
-    const response = await http.request(request);
+    request.method = method;
+  }
+}
 
-    return new Response(response.body, {
-      status: response.status,
+function addInitData(request: minecraftnet.HttpRequest, init?: RequestInit) {
+  if (init === undefined) return;
 
-      headers: response.headers.map((header): [string, string] => {
-        if (header.value instanceof String) {
-          return [header.key, header.value.toString()];
+  addInitBody(request, init);
+  addInitHeaders(request, init);
+  addInitMethod(request, init);
+}
+
+function requestFetch2MC(
+  input: RequestInfo | URL,
+  init?: RequestInit,
+): minecraftnet.HttpRequest {
+  let url = undefined;
+
+  if (input instanceof URL) url = input.host;
+  else if (typeof input === 'string') url = input;
+  else url = input.url;
+
+  let request = new minecraftnet.HttpRequest(url);
+
+  addInitData(request, init);
+
+  return request;
+}
+
+function responseMC2Fetch(response: minecraftnet.HttpResponse): Response {
+  return new Response(response.body, {
+    status: response.status,
+    headers: response.headers.map(
+      (header: minecraftnet.HttpHeader): [string, string] => {
+        if (typeof header.value === 'string') {
+          return [header.key, header.value];
         } else {
           return [header.key, ''];
         }
-      }),
-    });
-  } else {
-    return new Response();
-  }
-};
+      },
+    ),
+  });
+}
