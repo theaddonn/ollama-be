a7d01291689db8646534c7f327963170eef70f21
theaddon <theaddonn@gmail.com>
2025-02-18 00:12:19 +0100

    Add eval example tool

---
+++ packs/data/gametests/src/tools/eval.ts
@@ -0,0 +1,30 @@
+import { ToolFn } from '../tool';
+import Mexp from 'math-expression-evaluator';
+
+export class EvalFn implements ToolFn {
+  static readonly id: string = 'eval';
+  static readonly description: string =
+    'Evaluates a mathematical expression and returns the result as a string.';
+
+  required: string[] = [];
+  properties: {
+    [key: string]: { type: string; description: string; enum?: string[] };
+  } = {
+    expression: {
+      type: 'string',
+      description: 'The mathematical expression to evaluate',
+    },
+  };
+
+  private mexp = new Mexp();
+
+  handle(params: { [key: string]: any }): Promise<string> {
+    const expr = params['expression'] as string | undefined;
+
+    if (expr === undefined) throw Error('Missing property: expression');
+
+    let result = this.mexp.eval(expr);
+
+    return Promise.resolve(result.toString());
+  }
+}
